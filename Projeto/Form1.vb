442184c82233b3b583055aad49b64ee37d9bcb6d
P114 <P114@WIN-GESFLHLR4EE>
2019-10-16 13:11:33 +0100

    16-10-2019

--- Projeto/Form1.vb
+++ Projeto/Form1.vb
@@ -7,7 +7,9 @@
     Private Sub Clicar(sender As Object, e As EventArgs) Handles P4.Click, P1.Click, P9.Click, P8.Click, P7.Click, P6.Click, P5.Click, P3.Click, P2.Click, P16.Click, P15.Click, P14.Click, P13.Click, P12.Click, P11.Click, P10.Click
         Dim Aux = Quadros(Int(Rnd() * 16))
         Aux.BackgroundImage = My.Resources._1
-        P5.Visible = False
+
+
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
